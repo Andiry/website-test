0d5c7184c515ba130b41f32cf4162f6116d48567
Andiry Xu <jix024@cs.ucsd.edu>
2016-09-06 20:17:34 -0700

    live.html

--- scripts/global.js
+++ scripts/global.js
@@ -206,6 +206,41 @@ function prepareGallery() {
 	}
 }
 
+function stripeTables() {
+	var tables = document.getElementsByTagName("table");
+	if (!tables) return;
+
+	for (var i = 0; i < tables.length; i++) {
+		var odd = false;
+		var rows = tables[i].getElementsByTagName("tr");
+		for (var j = 0; j < rows.length; j++) {
+			if (odd == true) {
+				addClass(rows[j], "odd");
+				odd = false;
+			} else {
+				odd = true;
+			}
+		}
+	}
+}
+
+function highlightRows() {
+	var rows = document.getElementsByTagName("tr");
+	if (!rows) return;
+
+	for (var i = 0; i < rows.length; i++) {
+		rows[i].oldClassName = rows[i].className;
+		rows[i].onmouseover = function() {
+			addClass(this, "highlight");
+		}
+		rows[i].onmouseout = function() {
+			this.className = this.oldClassName;
+		}
+	}
+}
+
 addLoadEvent(prepareGallery);
+addLoadEvent(stripeTables);
+addLoadEvent(highlightRows);
 addLoadEvent(highlightPage);
 addLoadEvent(prepareSlideshow);
